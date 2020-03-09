3e8efec516458f9fafd26191a4a48361fef8ca50
s18606 <61777941+s18606@users.noreply.github.com>
2020-03-09 11:28:30 +0100

    potato_mode v.o.1

---
+++ projekt/Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    public float speed = 500;
+    public float jump = 400;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = false; 
+ 
+    }
+
+    // Update is called once per frame
+    void Update() { 
+        float xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+     rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
+
+        if (Input.GetKeyDown("space") && (isGrounded ==true)) {
+            rb.AddForce(new Vector2(0, jump));
+            isGrounded = false;
+            
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+}
+
+ 
